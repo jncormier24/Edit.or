276fa4d7e9ba02ddc66f8691422b4d5eaa9f2116
jncormier24 <jncormier24@gmail.com>
2013-07-06 20:01:34 -0400

    fixed autoload.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,6 +4,5 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 require_once __DIR__ . '/dBug.php';
-require_once '/Users/jncormier/Sites/git-checkout/Edit.or/vendor/Edit/Template/Template.php';
-
-return ComposerAutoloaderInit572a8464d9b22726e554faa993e09ba0::getLoader();
+require_once __DIR__ . '/Edit/Template/Template.php';
+return ComposerAutoloaderInit62268fb8609943692a101761577405bd::getLoader();
